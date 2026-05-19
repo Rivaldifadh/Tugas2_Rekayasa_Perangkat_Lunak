6ea3f9e2da52942abad8c6fd0493ff5fc28e1350
Rivaldi Fadilah <Rivaldibertiga@gmail.com>
2026-05-20 06:09:47 +0700

    Updates

---
+++ assets/status-tap-B_UDTYwV.js
@@ -0,0 +1,3 @@
+import{h as a,f as i,b as c,l as d,k as l}from"./index-DG6I81tL.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const h=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{h as startStatusTap};
